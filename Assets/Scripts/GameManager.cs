63e980cb5f4313939a6fdc5ccabe087c0b2691ce
Karl Gorgoglione <karl.gorgoglione@gmail.com>
2021-12-01 00:25:28 +0100

    Button to start a game after setting the pieces

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
+
+    [SerializeField]
+    Button startButton;
 
     public enum Mode { Placement, Game }
 
@@ -22,11 +26,12 @@ public class GameManager : MonoBehaviour
             Instance = this;
         }
         mode = Mode.Placement;
+        Debug.Log(SystemInfo.operatingSystem);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        startButton.enabled = mode == Mode.Placement;
     }
 }
